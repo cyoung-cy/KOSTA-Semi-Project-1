0aa205b9637e1dacf8899ff651f1b40008abdad3
hyuk-dev <hyuk.development@gmail.com>
2026-03-11 18:35:31 +0900

    빠진 부분 추가

--- MovieTicket/src/dao/MemberDAO.java
+++ MovieTicket/src/dao/MemberDAO.java
@@ -1,4 +1,17 @@
 package dao;
 
 public interface MemberDAO {
+	/*
+	 * 20260311
+	 * 이동혁
+	 * TODO:사용자 로그인 DAO 인터페이스 구현
+	 * */
+	Member login (String memberId)
+
+	/*
+	 * 20260311
+	 * 이동혁
+	 * TODO:사용자 회원가입 DAO 인터페이스 구현
+	 * */
+
 }
--- MovieTicket/src/dto/Member.java
+++ MovieTicket/src/dto/Member.java
@@ -1,4 +1,19 @@
 package dto;
-
+/**
+ * 2026-03-11
+ * 이동혁
+ * MemberDTO 구현
+ */
 public class Member {
+	private String userID;
+	private String password;
+	private String name;
+	private String phone;
+	private String address;
+	private String birthDate;
+	private String [] preferredGenre;
+	private String cardInfo;
+	private String role;
+	
+	
 }
