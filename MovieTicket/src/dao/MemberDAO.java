2acecebf56d6bdaeec11f94ffcea0cd18c8e37bc
cyoung <saidayummy@gmail.com>
2026-03-11 18:35:40 +0900

    0311 MemberDAO 수정

--- MovieTicket/src/dao/MemberDAO.java
+++ MovieTicket/src/dao/MemberDAO.java
@@ -1,4 +1,14 @@
 package dao;
 
+
+import dto.Member;
+
+import java.sql.SQLException;
+
 public interface MemberDAO {
+
+    /*
+     * 기능 : 시스템 실행시 제일 처음 보이는 StartView
+     * */
+    Member login(String userId, String userPwd)throws SQLException;
 }
--- MovieTicket/src/dao/MemberDAOImpl.java
+++ MovieTicket/src/dao/MemberDAOImpl.java
@@ -1,4 +1,12 @@
 package dao;
 
+import dto.Member;
+
+import java.sql.SQLException;
+
 public class MemberDAOImpl implements MemberDAO {
+    @Override
+    public Member login(String userId, String userPwd) throws SQLException {
+        return null;
+    }
 }
