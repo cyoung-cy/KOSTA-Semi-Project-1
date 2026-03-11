374d95a448fc07582ce4a5066117b0bb68d17341
cyoung <saidayummy@gmail.com>
2026-03-11 18:22:53 +0900

    0311 Startview

--- MovieTicket/src/view/StartView.java
+++ MovieTicket/src/view/StartView.java
@@ -6,9 +6,9 @@ import session.SessionSet;
 import java.util.Scanner;
 
 /*
- * 날짜
- * 이름
- * 수정 사항
+ * 날짜 : 0311
+ * 이름 : 김채영
+ * 수정 사항 : StartView(사용자, 관리자)
  * */
 
 public class StartView {
@@ -24,13 +24,12 @@ public class StartView {
             int menu = Integer.parseInt(sc.nextLine());
             switch(menu) {
                 case 1 :
-                    StartView.login();// 로그인 - 만약 role=user이면 printUserMenu()로
+                    // 로그인
+                    StartView.login();
                     break;
                 case 2 :
                     // 회원가입
-                    break;
-                case 3 :
-                    // 관리자 회원가입 ACCESSCODE 받기
+                    StartView.signUp();
                     break;
                 case 0 :
                     System.exit(0);
@@ -39,21 +38,25 @@ public class StartView {
 
     }
 
-
+    /*
+     * 기능 : 시스템 실행시 제일 처음 보이는 StartView
+     * */
     public static void printMenu() {
         System.out.println("=============================================================");
         System.out.println("                 Hello! Welcome MOVIE TICKET                 ");
         System.out.println("=============================================================");
         System.out.println("                      [1] 로그인");
         System.out.println("                      [2] 회원가입");
-        System.out.println("                      [3] 관리자 회원가입");
         System.out.println("                      [0] 프로그램 종료");
         System.out.println("=============================================================");
         System.out.println("이용하실 서비스를 선택하세요: ");
 
     }
 
-
+    /*
+     * 기능 : userId='user'일 때 사용자 View
+     * @param : userId(String)
+     * */
     public static void printUserMenu(String userId) {
         while(true) {
             SessionSet ss = SessionSet.getInstance();
@@ -86,6 +89,7 @@ public class StartView {
                     //문의하기
                 case 6 :
                     //로그아웃
+                    StartView.logout(userId);
                 case 7 :
                     //회원탈퇴
                 case 0 :
@@ -107,6 +111,10 @@ public class StartView {
         return String.format(format, text);
     }
 
+    /*
+     * 기능 : userId='admin' or 'subadmin' 일 때 관리자 View
+     * @param : userId(String)
+     * */
     public static void printAdminMenu(String userId) {
         while(true){
             SessionSet ss = SessionSet.getInstance();
@@ -118,9 +126,6 @@ public class StartView {
             System.out.println("                      [1] 회원 관리");
             System.out.println("                      [2] 영화 관리");
             System.out.println("                      [3] 문의 관리");
-            if(userId == "King"){
-                System.out.println("                      [4] 권한 코드 부여");
-            }
             System.out.println("                      [0] 프로그램 종료");
             System.out.println("=============================================================");
 
@@ -132,12 +137,6 @@ public class StartView {
                     //영화 관리
                 case 3 :
                     //문의 관리
-                case 4 :
-                    if(userId != "King"){
-                        System.out.println("접근 권한이 없습니다.");
-                    }else{
-                        //권한 코드 부여
-                    }
                 case 0 :
                     //종료
                     System.exit(0);
@@ -147,25 +146,58 @@ public class StartView {
 
     }
 
-    /**
-     * 로그인 메뉴
+    /*
+     * 기능 : 로그인
      * */
     public static void login() {
         System.out.print("아이디 : ");
         String userId = sc.nextLine();
 
         System.out.print("비밀번호 : ");
-        String userPwd = sc.nextLine();
+        String password = sc.nextLine();
+
+        //MemberController.login(userId, );
     }
 
 
-    /**
-     * 로그아웃
+    /*
+     * 기능 : 로그아웃
      * */
     public static void logout(String userId) {
         Session session = new Session(userId);
         SessionSet ss = SessionSet.getInstance();
         ss.remove(session);
+    }
+
+    /*
+     * 기능 : 회원가입
+     * */
+    public static void signUp() {
+        System.out.print("아이디 : ");
+        String userId = sc.nextLine();
+
+        System.out.print("비밀번호 : ");
+        String password = sc.nextLine();
+
+        System.out.print("이름 : ");
+        String name = sc.nextLine();
+
+        System.out.print("전화번호 : ");
+        String phone = sc.nextLine();
+
+        System.out.print("주소 : ");
+        String address = sc.nextLine();
+
+        System.out.print("생일 : ");
+        String birth = sc.nextLine();
+
+        System.out.print("선호 장르 : ");
+        String prferredGenre = sc.nextLine();
+
+        System.out.print("결제 정보 : ");
+        String cardInfo = sc.nextLine();
+
+        //MemberController.login(userId, );
     }
 }
 
